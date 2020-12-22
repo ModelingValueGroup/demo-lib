b13608c4d4bb2fac508893d756ecd7e134770c8f
Tom Brus <tombrus@gmail.com>
2020-12-22 17:27:05 +0100

    same gradle on master

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,6 +9,8 @@ val CI: Boolean = "true".equals(System.getenv("CI"))
 val TOKEN: String = System.getenv("TOKEN") ?: "DRY"
 val GITHUB_REF: String = System.getenv("GITHUB_REF") ?: "local"
 
+println("GITHUB_REF=$GITHUB_REF")
+
 group = "demo-lib"
 version = "2.0.0"
 
