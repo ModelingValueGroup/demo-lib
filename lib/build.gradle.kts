da4dd7808743405f5c5c77727cdf2bcb5b5b72a8
Tom Brus <tombrus@gmail.com>
2020-12-27 00:14:46 +0100

    add our own plugin

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -39,35 +39,25 @@ println("@@@@@@@@@@@          group=$group")
 println("@@@@@@@@@@@    packageRepo=$packageRepo")
 
 plugins {
-    // Apply the java-library plugin for API and implementation separation.
     `java-library`
     `maven-publish`
     id("org.modelingvalue.gradle.corrector") version "0.3.30"
 }
 
-rootProject.defaultTasks("mvgCorrector","clean", "build", "publish", "mvgTagger")
+rootProject.defaultTasks("clean", "build", "publish", "mvgTagger")
 
 repositories {
-    // Use JCenter for resolving dependencies.
     jcenter()
 }
 
 dependencies {
-    // Use JUnit Jupiter API for testing.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-
-    // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation("com.google.guava:guava:29.0-jre")
 }
 
 tasks.test {
-    // Use junit platform for unit tests.
     useJUnitPlatform()
 }
 
