a2cd2a9d82896b2a89298e3986125203ed4a155c
Tom Brus <tombrus@gmail.com>
2021-01-05 00:04:39 +0100

    revert: 42 and de.fayard.buildSrcVersions

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -34,10 +34,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    api("org.apache.commons:commons-math3")
-    implementation("com.google.guava:guava")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    api("org.apache.commons:commons-math3:3.7.0")
+    implementation("com.google.guava:guava:29.0-jre")
 }
 
 tasks.test {
